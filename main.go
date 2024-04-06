31d5894db8c3ca927e6cd0f1b87fd65f28355f12
Arnau <arnauapps@gmail.com>
2024-04-06 22:51:29 +0200

    Server can now parse request line and extract http method, resource and version

--- main.go
+++ main.go
@@ -3,27 +3,44 @@ package main
 import (
 	"log"
 	"net"
+	"strings"
 )
 
+func handleConn(conn net.Conn) {
+	// Close connection after handling it
+	defer conn.Close()
+
+	// Read http connection
+	buffer := make([]byte, 2048)
+	n, err := conn.Read(buffer)
+	if err != nil {
+		log.Printf("ERROR: Could not read from connection [%s]", conn.RemoteAddr().String())
+		conn.Close()
+	}
+
+	lines := strings.Split(string(buffer[:n]), "\n")
+
+	// Parse http request line parameters to determine which file to serve
+	httpRequestLineParams := strings.Split(lines[0], " ")
+	for i, param := range httpRequestLineParams {
+		log.Printf("Parameter %d: %s", i, param)
+	}
+}
+
 func main() {
+	// Listen and create a TCP server in localhost:port
 	ln, err := net.Listen("tcp", "127.0.0.1:6969")
 	if err != nil {
 		log.Fatalf("An error occured while starting the server: %s", err)
 	}
 
+	// Accept every incoming connection and handle it properly concurrently
 	for {
 		conn, err := ln.Accept()
 		if err != nil {
 			log.Printf("ERROR: Could not accept connection %s", conn.RemoteAddr().String())
 		}
 		log.Printf("Accepted connection from [%s]", conn.RemoteAddr().String())
-		buffer := make([]byte, 2048)
-		n, err := conn.Read(buffer)
-		if err != nil {
-			log.Printf("ERROR: Could not read from connection [%s]", conn.RemoteAddr().String())
-		}
-
-		log.Printf("Connection body: \n %s", buffer)
-		log.Printf("Connection length %d", n)
+		go handleConn(conn)
 	}
 }
